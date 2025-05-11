f9295028e7c5bd4a2d7550bc1dafebf44068c6e6
CENTURION 1 <kdtkachenko@gmail.com>
2025-05-11 23:33:19 +0300

    Добавьте файлы проекта.

---
+++ UserService.Core/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿namespace UserService.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.student;
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>(); 
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+    }
+
+    public enum UserRole
+    {
+        student,
+        teacher,
+        staff,
+        admin
+    }
+}
---
+++ UserService.Infrastructure/DbContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Core.Entities;
+
+namespace UserService.Infrastructure.DbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+       
+    }
+}
